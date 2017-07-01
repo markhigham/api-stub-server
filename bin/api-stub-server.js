605105cfdfb2e4be63a0d371fe1a8df95e57c227
Mark Higham <mark.higham@gmail.com>
2017-07-01 17:17:00 +0100

    reorganized

--- bin/api-stub-server.js
+++ bin/api-stub-server.js
@@ -3,6 +3,7 @@
 process.env.DEBUG = process.env.DEBUG || "api*";
 process.env.DEBUG_LEVEL = process.env.DEBUG_LEVEL || "info";
 
+const colors = require('colors');
 const app = require('../lib/api');
 const config = require('../config');
 const fs = require('fs');
@@ -11,6 +12,33 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const port = argv.p || config.port;
 const host = argv.h || config.host;
+
+function showHelp() {
+    console.log(`api-stub-server [-p 8092] [-h 127.0.0.1] [saved_response_file.json]
+-p  (Optional) Port number - defaults to 3001
+
+-h  (Optional) Host address - defaults to 0.0.0.0
+
+saved_response_file.json (optional)
+    Path to a file containing pre-saved responses
+
+    `);
+}
+
+if (argv.h) {
+    showHelp();
+    process.exit(0);
+}
+
+if (isNaN(port)) {
+    showHelp();
+    process.exit(-1);
+}
+
+process.on('uncaughtException', (err) => {
+    console.error('Something unexpected happened. See the error code below'.red);
+    console.error(err);
+});
 
 app.start(port, host).then(() => {
     if (argv._.length === 0) {
@@ -27,7 +55,7 @@ app.start(port, host).then(() => {
 }).then(() => {
     // started
 }).catch(err => {
-    console.error(err);
+    console.error('Something failed');
     process.exit(-1);
 });
 
