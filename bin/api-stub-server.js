f4d085c7826f980ac4f42f844f49ebf348aeea93
Mark Higham <mark.higham@dxc.com>
2018-06-14 17:07:38 +0100

    adding prettier and preparation for multi-tenant

--- bin/api-stub-server.js
+++ bin/api-stub-server.js
@@ -2,13 +2,12 @@
 
 process.env.DEBUG = process.env.DEBUG || "api*";
 
+const colors = require("colors");
+const app = require("../lib/api");
+const config = require("../config");
+const fs = require("fs");
 
-const colors = require('colors');
-const app = require('../lib/api');
-const config = require('../config');
-const fs = require('fs');
-
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 const port = argv.p || config.port;
 const host = argv.h || config.host;
@@ -17,7 +16,7 @@ const debugLevel = argv.v || process.env.DEBUG_LEVEL || "info";
 process.env.DEBUG_LEVEL = debugLevel;
 
 function showHelp() {
-    console.log(`api-stub-server [-p 8092] [-h 127.0.0.1] [-v verbose] [saved_response_file.json]
+  console.log(`api-stub-server [-p 8092] [-h 127.0.0.1] [-v verbose] [saved_response_file.json]
 -p  (Optional) Port number - defaults to 3001
 
 -h  (Optional) Host address - defaults to 0.0.0.0
@@ -31,40 +30,39 @@ saved_response_file.json (optional)
 }
 
 if (argv.h) {
-    showHelp();
-    process.exit(0);
+  showHelp();
+  process.exit(0);
 }
 
 if (isNaN(port)) {
-    showHelp();
-    process.exit(-1);
+  showHelp();
+  process.exit(-1);
 }
 
-process.on('uncaughtException', (err) => {
-    console.error('Something unexpected happened. See the error code below'.red);
-    console.error(err);
+process.on("uncaughtException", err => {
+  console.error("Something unexpected happened. See the error code below".red);
+  console.error(err);
 });
 
-app.start(port, host).then(() => {
+app
+  .start(port, host)
+  .then(() => {
     console.log(`debugging is ${debugLevel}`);
     if (argv._.length === 0) {
-        return;
+      return;
     }
 
     const filename = argv._[0];
     console.log(`using ${filename}`);
 
-    var file = fs.readFileSync(filename, 'utf8');
+    var file = fs.readFileSync(filename, "utf8");
     var json = JSON.parse(file);
     return app.upload(json);
-
-}).then(() => {
+  })
+  .then(() => {
     // started
-}).catch(err => {
-    console.error('Something failed');
+  })
+  .catch(err => {
+    console.error("Something failed");
     process.exit(-1);
-});
-
-
-
-
+  });
