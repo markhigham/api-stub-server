6a59d56fd928e4b8c548331eeef1ec201c64160d
Mark Higham <mark.higham@gmail.com>
2022-01-21 21:01:04 +0000

    more loggin

--- bin/api-stub-server.js
+++ bin/api-stub-server.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
 const argv = require("minimist")(process.argv.slice(2));
-const debugLevel = argv.v || process.env.DEBUG_LEVEL || "info";
-process.env.DEBUG_LEVEL = debugLevel;
 
-const colors = require("colors");
 const app = require("../lib/api");
 const config = require("../lib/config");
 const fs = require("fs");
@@ -12,7 +9,7 @@ const fs = require("fs");
 const port = argv.p || config.port;
 const host = argv.h || config.host;
 
-const logger = require("../lib/logger")(__filename)
+const logger = require("../lib/logger")(__filename);
 
 function showHelp() {
   console.log(`api-stub-server [-p 8092] [-h 127.0.0.1] [-v verbose] [-s use sample data] [-r x] [saved_response_file.json]
@@ -21,8 +18,6 @@ version: ${config.buildNumber}
 -p  (Optional) Port number - defaults to 3001
 
 -h  (Optional) Host address - defaults to 0.0.0.0
-
--v  (Optional) Verbosity - choose from log, error, warn, debug, info
 
 -s  (Optional) Use sample data
 
@@ -46,14 +41,14 @@ if (isNaN(port)) {
 }
 
 process.on("uncaughtException", (err) => {
-  console.error("Something unexpected happened. See the error code below".red);
+  console.error("Something unexpected happened. See the error code below");
   console.error(err);
 });
 
 app
   .start(port, host)
   .then(() => {
-    logger.info(`debugging is ${debugLevel}`);
+    logger.info(`log level ${process.env.LOG_LEVEL}`);
 
     if (argv.r) {
       let limit = isNaN(argv.r) ? 0 : argv.r;
@@ -70,17 +65,22 @@ app
 
     let filename;
     if (process.env.SAVED_RESPONSE_FILE) {
-      console.log(`using ${process.env.SAVED_RESPONSE_FILE}`);
+      logger.info(`using ${process.env.SAVED_RESPONSE_FILE}`);
       filename = process.env.SAVED_RESPONSE_FILE;
     }
 
     // This means that arg will override environment variable
     if (argv._.length) {
       filename = argv._[0];
-      console.log(`using ${filename}`);
+      logger.info(`using ${filename}`);
     }
 
     if (!filename) return;
+
+    if (!fs.existsSync(filename)) {
+      console.error(`${filename} does not exist`);
+      process.exit(-1);
+    }
 
     const file = fs.readFileSync(filename, "utf8");
     const json = JSON.parse(file);
