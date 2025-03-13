83ab8aa2c16b6f78ea7138534c3a5cf2f7cb9c0e
Mark Higham <mark.higham@gmail.com>
2025-03-13 13:51:41 +0000

    Add seconds and make relative to service start

--- src/lib/interpolator.ts
+++ src/lib/interpolator.ts
@@ -2,6 +2,9 @@ import * as clone from 'clone'
 
 import { config } from './config'
 import { IResponse } from './interfaces'
+import * as moment from "moment";
+
+const baselineTimer = new Date()
 
 export class ResponseInterpolator {
   static interpolate(
@@ -9,7 +12,9 @@ export class ResponseInterpolator {
     matchResult: { [p: string]: string },
     tenant: string = '',
   ): any {
-    const ticks = new Date().getTime().toString()
+    const now = new Date()
+    const ticks = now.getTime() - baselineTimer.getTime()
+    const seconds = moment(now).diff(moment(baselineTimer), 'seconds')
     const count = String(++response.count)
 
     const body = clone(response.body)
@@ -19,7 +24,8 @@ export class ResponseInterpolator {
     }
 
     const json = JSON.stringify(body)
-    let replaced = json.replace(/{{\$ticks}}/g, ticks)
+    let replaced = json.replace(/{{\$ticks}}/g, ticks.toString())
+    replaced = replaced.replace(/{{\$seconds}}/g, seconds.toString())
     replaced = replaced.replace(/{{\$count}}/g, count)
     replaced = replaced.replace(/{{\$uid}}/g, response.uid)
     replaced = replaced.replace(/{{\$tenant}}/g, tenant)
